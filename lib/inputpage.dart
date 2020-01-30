f82c095d2529fc88142f3c2bc72ac04750afff2d
Swaraj961 <swarajroutray961@gmail.com>
2020-01-30 21:21:55 +0530

    i

--- lib/inputpage.dart
+++ lib/inputpage.dart
@@ -18,11 +18,21 @@ class InputPage extends StatelessWidget {
               child: Row( // for row  boxes -2
                 children: <Widget>[
                   Expanded(child: ReusableCard(colour: colourofblock,
+                  cardchild: IconContent(
+                    mficons: FontAwesomeIcons.mars,
+                    label: 'MALE',
+
+                  ),
                   
                   
                 ),
                   ),
-                  Expanded(child:ReusableCard(colour: colourofblock,),
+                  Expanded(child:ReusableCard(colour: colourofblock,
+                  cardchild:IconContent(
+                    mficons: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
+                  ),
                   ),
                 ],
               ),
@@ -48,6 +58,38 @@ class InputPage extends StatelessWidget {
             ) ,
           ],
         ),
+    );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  const IconContent({ this.mficons, this.label
+    
+   
+  });
+   final IconData mficons;
+   final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          mficons,
+          size: 80.0,
+          color: Colors.white,
+
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+       Text(
+         label,
+       style: TextStyle(fontSize: 18,
+       color: Color(0xFF8D8E98),),
+       ),
+      ],
     );
   }
 }
